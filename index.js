bf732afb5b50e61e9a420c26cef88779d922cc2a
dongwenxiao <cs_victor@126.com>
2018-10-23 11:55:03 +0800

    修复文件复制错了！！！

--- index.js
+++ index.js
@@ -1,314 +1,171 @@
-const fs = require('fs')
-const path = require('path')
-const webpack = require('webpack')
-// const ExtractTextPlugin = require("extract-text-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const postcss = require('postcss')
+const loaderUtils = require("loader-utils")
+const md5 = require('md5')
+const collection = []
 
-const pwaCreatePlugin = require('sp-pwa')
+let sameIndex = 0
 
-// 打包结果目录
-const outputPath = 'dist'
+module.exports = function(content) {
 
-// 执行顺序，从右到左
-const factory = async ({
-    aliases,
-    env, stage,
-    // spa = false,
-    defines = {},
-}) => {
-    const useSpCssLoader = 'sp-css-loader?length=2&mode=replace&readable=true'
-    const useUniversalAliasLoader = {
-        loader: "universal-alias-loader",
-        options: {
-            alias: aliases
+    this.cacheable && this.cacheable()
+
+    content = content.replace(/\'/g, '\"')
+
+    // md5后，class名字长度
+    // 默认5个字符
+    let query = loaderUtils.parseQuery(this.query)
+
+    // replace|wrapper|none
+    // replace 是否把样式替换md5值，替换 .page => .c2d3
+    // wrapper 是把样式外包一层， .page => .c2d3 .page
+    // none 是什么都不做，md5仍然做  .page => .page
+    let mode = query.mode
+
+    // 是否可读
+    // true => .name_d3ef
+    // false=> .d3ef
+    let readable = query.readable
+
+    // md5 字符串wrapper的class名长度
+    let length = query.length || 5
+
+    // class名字自定义规则
+    // TODO: 目前只支持1个规则: .component__[name]__ 其他规则用到的时候再扩展
+    // .component => .af2e
+    // .component__header__ => .header_af2e
+    // let pattern = query.pattern || '.component__[name]__'
+
+    let md5Name = md5(content)
+    let customNameMd5 = ''
+
+    // 强制第一位是字母
+    let firstChat = md5Name.match(/[a-zA-Z]{1}/)[0]
+
+    // md5 后去掉length-1个字符
+    let otherChats = md5Name.substr(0, length - 1)
+
+    // 以字符开通的class名
+    md5Name = firstChat + otherChats
+
+    // 去重
+    let flag = true
+    while (flag) {
+        flag = !!~collection.indexOf(md5Name)
+        if (flag) {
+            md5Name += (sameIndex + '')
+            sameIndex++
         }
     }
 
-    const extractCSS = process.env.WEBPACK_BUILD_ENV === 'prod' ? true : false
+    let customName = ''
 
-    return {
-        module: {
-            rules: [
-                // {
-                //     test: /\.json$/,
-                //     loader: 'json-loader'
-                // },
+    //
+    if (mode === 'wrapper') {
+        // 暂无实现
+    } else if (mode === 'none') {
+        // 暂无实现
+    } else if (mode === 'replace') {
 
-                // CSS - general
-                {
-                    test: /\.css$/,
-                    exclude: [/\.g\.css$/, /node_modules/],
-                    use: [
-                        useSpCssLoader,
-                        "postcss-loader",
-                        useUniversalAliasLoader
-                    ]
-                }, {
-                    test: /\.less$/,
-                    exclude: [/\.g\.less$/, /node_modules/],
-                    use: [
-                        useSpCssLoader,
-                        "postcss-loader",
-                        "less-loader",
-                        useUniversalAliasLoader
-                    ]
-                }, {
-                    test: /\.scss$/,
-                    exclude: [/\.g\.scss$/, /node_modules/],
-                    use: [
-                        useSpCssLoader,
-                        "postcss-loader",
-                        "sass-loader",
-                        useUniversalAliasLoader
-                    ]
-                },
+        // postcss 处理每一个class名字
+        let root = postcss.parse(content)
+        let once = true // 处理名字只处理1次
+        root.walkRules((rule, i) => {
 
-                // CSS - in node_modules
-                {
-                    test: /\.css$/,
-                    include: /node_modules/,
-                    use: [
-                        "style-loader",
-                        "postcss-loader"
-                    ]
-                }, {
-                    test: /\.less$/,
-                    include: /node_modules/,
-                    use: [
-                        "style-loader",
-                        "postcss-loader",
-                        "less-loader"
-                    ]
-                }, {
-                    test: /\.scss$/,
-                    include: /node_modules/,
-                    use: [
-                        "style-loader",
-                        "postcss-loader",
-                        "sass-loader"
-                    ]
-                },
+            // 排除@keyframe
+            if (rule.parent.type == 'atrule' && rule.parent.name == 'keyframes')
+                return
 
-                // CSS - critical
-                {
-                    test: extractCSS ? /critical\.g\.css$/ : /^IMPOSSIBLE$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "postcss-loader",
-                        useUniversalAliasLoader
-                    ]
-                    // use: ExtractTextPlugin.extract({
-                    //     fallback: "style-loader",
-                    //     use: ["css-loader", "postcss-loader"]
-                    // })
-                }, {
-                    test: extractCSS ? /critical\.g\.less$/ : /^IMPOSSIBLE$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "postcss-loader",
-                        "less-loader",
-                        useUniversalAliasLoader
-                    ]
-                    // use: ExtractTextPlugin.extract({
-                    //     fallback: "style-loader",
-                    //     use: ["css-loader", "postcss-loader", "less-loader"]
-                    // })
-                }, {
-                    test: extractCSS ? /critical\.g\.scss$/ : /^IMPOSSIBLE$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "postcss-loader",
-                        "sass-loader",
-                        useUniversalAliasLoader
-                    ]
-                    // use: ExtractTextPlugin.extract({
-                    //     fallback: "style-loader",
-                    //     use: ["css-loader", "postcss-loader", "sass-loader"]
-                    // })
-                },
+            // 每个class外面加1层class
+            rule.selectors = rule.selectors.map(selector => {
 
-                // CSS - other global
-                {
-                    test: /\.g\.css$/,
-                    exclude: extractCSS ? /critical\.g\.css$/ : undefined,
-                    // loader: 'style-loader!postcss-loader'
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        "postcss-loader",
-                        useUniversalAliasLoader
-                    ]
-                }, {
-                    test: /\.g\.less$/,
-                    exclude: extractCSS ? /critical\.g\.less$/ : undefined,
-                    // loader: 'style-loader!postcss-loader!less-loader'
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        "postcss-loader",
-                        "less-loader",
-                        useUniversalAliasLoader
-                    ]
-                }, {
-                    test: /\.g\.scss$/,
-                    exclude: extractCSS ? /critical\.g\.scss$/ : undefined,
-                    // loader: 'style-loader!postcss-loader!sass-loader'
-                    use: [
-                        "style-loader",
-                        "css-loader",
-                        "postcss-loader",
-                        "sass-loader",
-                        useUniversalAliasLoader
-                    ]
-                },
+                // 可读性好的class名字
+                // eg: .app_3fea
 
-                //
+                if (~selector.indexOf('__component')) {
 
-                {
-                    test: /\.(js|jsx)$/,
-                    loader: 'babel-loader'
+                    // 可读性处理
+                    let readablePatten = new RegExp('.[a-zA-Z0-9]+__component')
+                    if (readable) {
+                        let name = selector.match(readablePatten)[0]
+
+                        // 去下划线前部分
+                        customName = name.split('__')[0]
+
+                        // 去掉第一个点
+                        if (customName.charAt(0) == '.') customName = customName.replace('.', '')
+
+                        // 可读class名拼接md5字符串
+                        customNameMd5 = customName + '_' + md5Name
+                        let patten = new RegExp(name, 'g')
+                        let result = selector.replace(patten, '.' + customNameMd5)
+
+                        return result
+                    } 
+                    // 不可读性处理
+                    else {
+                        selector = selector.replace(readablePatten, '.component')
+                        return simpleReplace(selector, md5Name)
+                    }
                 }
-            ]
-        },
-        resolve: {
-            alias: { ...aliases },
-            modules: [
-                '__modules',
-                'node_modules'
-            ],
-            extensions: ['.js', '.jsx', '.json', '.css', '.less', '.sass', '.scss']
-        },
-        plugins: plugins(env, stage, defines)
-    }
-}
 
+                // class名字直接用md5值替换，可读性不好，用于压缩
+                else if (~selector.indexOf('.component')) {
+                    return simpleReplace(selector, md5Name)
+                } else {
+                    return `.${md5Name} ${selector}`
+                }
 
-// 执行顺序, 先 -> 后
-const plugins = (env, stage, defines = {}) => {
+                function simpleReplace(selector, md5Name) {
+                    return selector.replace(/.component/g, '.' + md5Name)
+                }
+            })
+        })
 
-    let g = Object.assign({
-        '__CLIENT__': stage == 'client',
-        '__SERVER__': stage == 'server',
-        '__DEV__': env == 'dev',
-        '__PROD__': env == 'prod',
-        // '__SPA__': !!spa,
-        // '__DIST__': JSON.stringify(process.env.SUPER_DIST_DIR),
-        '__DIST__': JSON.stringify(path.resolve(process.cwd(), process.env.SUPER_DIST_DIR)),
-        __SERVER_PORT__: JSON.stringify(process.env.SERVER_PORT),
-    }, defines)
+        handleBackground(root)
 
-    if (env == 'prod') {
-        process.env.NODE_ENV = 'production'
-        // g['process.env'] = {
-        //     'NODE_ENV': JSON.stringify('production')
-        // }
-    }
+        // 导出md5的class名字和处理后的css文本
+        // 把单引号统一处理成双引号 "" -> ''
 
-    const envs = [
-        'SUPER_DIST_DIR',
-        'SUPER_I18N',
-        'SUPER_I18N_TYPE',
-        "SUPER_I18N_LOCALES",
-        "SUPER_I18N_COOKIE_KEY",
-        "SUPER_I18N_COOKIE_DOMAIN",
-        "SUPER_HTML_TEMPLATE",
-        "WEBPACK_CHUNKMAP",
-        // "WEBPACK_SERVER_PUBLIC_PATH",
-    ]
-    const envsToDefine = envs.filter(key => (
-        typeof process.env[key] !== 'undefined'
-    ))
+        let fileId = customNameMd5 || md5Name
+        let result = `module.exports = {
+            wrapper: '${fileId}',
+            css: '${root.toString()}'
+        }`
 
-    return [
-        new webpack.DefinePlugin(g),
-        new webpack.EnvironmentPlugin(envsToDefine),
-    ]
-}
+        // result = result.replace(/\r\n/gi, '').replace(/\n/gi, '')
+        result = result
+            .replace(/(\r\n)|(\n)|(\/\*[\S\s]+?\*\/)|(\/\/)|(\s*;\s*(})\s*)|(\s*([{},;:])\s*)/gi, '$6$8')
+            .replace(/\s{2,}/gi, ' ')
 
-const factoryPWAPlugin = (opt) => {
-
-    let config = {
-        outputPath: '',//path.resolve(opt.outputPath, '../'),  // 子应用打包后文件夹的上一级
-        outputFilename: `service-worker.${opt.appName}.js`,
-        // customServiceWorkerPath: path.normalize(appPath + '/src/client/custom-service-worker.js'),
-        globPattern: `/${opt.appName}/**/*`,
-        // globOptions: {
-        //     ignore: [
-        //         '/**/portals/',
-        //         '/**/portals/**/*'
-        //     ]
-        // }
+        return result
+    } else {
+        // 暂无实现
     }
 
-    Object.assign(config, opt)
-
-    return pwaCreatePlugin(config)
+    collection.push(md5Name)
 }
 
-const resolve = Object.assign({
-    modules: [
-        '__modules',
-        'node_modules'
-    ],
-    alias: {
-        // Apps: path.resolve(appPath, './apps'),
-        // "@app": path.resolve(appPath, './apps/app')
-    },
-    extensions: ['.js', '.jsx', '.json', '.css', '.less', '.sass', '.scss']
-})
+function handleBackground(root) {
 
+    // 处理背景图片
+    root.walkDecls(/^(background|border)/, decl => {
 
-// 这里配置需要babel处理的node_modules
-// 大部分是自己用es6语法写的模块
-const needBabelHandleList = [
-    'super-project',
-    'sp-base',
-    'sp-boilerplate',
-    'sp-css-import',
-    'sp-css-loader',
-    'sp-mongo',
-    'sp-api',
-    'sp-cors-middleware',
-    'sp-react-isomorphic',
-    'sp-model',
-    'sp-cms',
-    'sp-auth',
-    'sp-koa-views',
-    'sp-response',
-    'sp-upload',
-    'sp-i18n',
-    'super-i18n',
-    'super-ui-pagecontainer',
-]
+        // 匹配到background中的url()
+        let matches = decl.value.match(/url\((.*?)\)/)
 
-// https://github.com/webpack/webpack/issues/2852
-// webpack 在打包服务端依赖 node_modules 的时候易出错，
-// 所以把 package.json 里描述的依赖过滤掉，只打包自己写的代码
-// 注：在上线的时候需要需要自行安装 package.json 的依赖包
-const filterExternalsModules = () => fs
-    // .readdirSync(path.resolve(__dirname, '../../', 'node_modules'))
-    .readdirSync(path.resolve(__dirname, '../../../'))
-    .concat(['react-dom/server'])
-    .filter((x) => ['.bin'].concat(needBabelHandleList).indexOf(x) === -1)
-    .filter((x) => !/^sp-/.test(x))
-    .filter((x) => !/^super-/.test(x))
-    .reduce((ext, mod) => {
-        ext[mod] = ['commonjs', mod].join(' ') // eslint-disable-line no-param-reassign
-        return ext
-    }, {})
+        if (matches && matches.length > 1) {
+            let v = matches[1]
 
-// 已下属都可以在 /config/webpack.js 中扩展
-module.exports = {
-    factory,
+            decl.value = decl.value.replace(v, (m) => {
 
-    outputPath,
-    // rules,
-    plugins,
-    factoryPWAPlugin,
-    resolve,
-    needBabelHandleList,
-    filterExternalsModules
+                // 双引号变单引号
+                m = m.replace(/\"/g, '\'')
+                if (m.indexOf('\'') < 0) {
+                    m = `\'${m}\'`
+                }
+
+                return "' +  require(" + m + ") + '"
+            })
+        }
+    })
 }
